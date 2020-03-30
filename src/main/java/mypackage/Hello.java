f67430ea76f44f6104511536855c16f858ec647e
fukumasa <dev.fukumasa@gmail.com>
2020-03-30 23:20:10 +0900

    Initial Commit

---
+++ src/main/java/mypackage/Hello.java
@@ -0,0 +1,22 @@
+package mypackage;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello") //[1]
+public class Hello extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<html><head></head><body>");
+		out.println("<p>Hello World!</p>");
+		out.println("</body></html>");
+	}
+}
